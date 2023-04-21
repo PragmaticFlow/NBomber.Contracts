7ccbf5f9b93dc159a35feffecee59ec28cbe2333
antyadev <antyadev@gmail.com>
2023-04-21 09:17:14 +0300

    added MetricsStats

---
+++ src/NBomber.Contracts/Metrics.fs
@@ -0,0 +1,31 @@
+﻿namespace NBomber.Contracts.Stats
+
+open System
+open MessagePack
+
+[<CLIMutable>]
+[<MessagePackObject>]
+type MetricPercentiles = {
+    [<Key 0>] Mean: float
+    [<Key 1>] Max: float
+    [<Key 2>] Percent50: float
+    [<Key 3>] Percent75: float
+    [<Key 4>] Percent95: float
+    [<Key 5>] Percent99: float    
+}
+
+type MetricType =
+    | Histogram = 0
+    | Gauge = 1
+
+[<CLIMutable>]
+[<MessagePackObject>]
+type MetricStats = {
+    [<Key 0>] Name: string
+    [<Key 1>] MeasureUnit: string
+    [<Key 2>] MetricType: MetricType
+    [<Key 3>] Current: float
+    [<Key 4>] Max: float
+    [<Key 5>] Duration: TimeSpan
+    [<Key 6>] Percentiles: MetricPercentiles option
+}
