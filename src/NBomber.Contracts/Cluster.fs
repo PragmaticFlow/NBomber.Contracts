aa94c83901cab9062501657c4cace058db4d5dd7
antyadev <antyadev@gmail.com>
2025-04-27 16:30:34 +0300

    Added ClusterInfo

---
+++ src/NBomber.Contracts/Cluster.fs
@@ -0,0 +1,14 @@
+﻿namespace NBomber.Contracts.Cluster
+
+open System
+open MessagePack
+open NBomber.Contracts.Stats
+
+[<CLIMutable; MessagePackObject>]
+type ClusterInfo = {
+    [<Key 0>] StartTime: DateTime
+    [<Key 1>] Duration: TimeSpan
+    [<Key 2>] CurrentDuration: TimeSpan
+    [<Key 3>] CurrentOperation: OperationType
+}
+
