d9a8699e1fcaf038d07cb97bf827556b8d368fd8
antyadev <antyadev@gmail.com>
2025-05-07 15:06:23 +0300

    Added ClusterCommand

--- src/NBomber.Contracts/Cluster.fs
+++ src/NBomber.Contracts/Cluster.fs
@@ -4,6 +4,11 @@ open System
 open MessagePack
 open NBomber.Contracts.Stats
 
+type ClusterCommand =
+    | StopScenario of scenarioName:string * reason:string
+    | StopTest     of reason:string
+    | GetClusterInfo
+
 [<CLIMutable; MessagePackObject>]
 type ClusterInfo = {
     [<Key 0>] StartTime: DateTime
