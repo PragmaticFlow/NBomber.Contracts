b8cf5fb3393d77bb3cddb1a8c5ce5d76d3d6c888
antyadev <antyadev@gmail.com>
2025-05-08 19:39:36 +0300

    Added Cluster MessageHeaders

--- src/NBomber.Contracts/Cluster.fs
+++ src/NBomber.Contracts/Cluster.fs
@@ -4,6 +4,20 @@ open System
 open MessagePack
 open NBomber.Contracts.Stats
 
+[<CLIMutable; MessagePackObject>]
+type MessageHeaders = {
+    [<Key 0>] CorrelationId: string
+    [<Key 1>] ClientId: string
+    [<Key 2>] SessionId: string
+    [<Key 3>] ResponseTopic: string option
+}
+
+[<CLIMutable; MessagePackObject>]
+type Message<'T> = {
+    [<Key 0>] Headers: MessageHeaders
+    [<Key 1>] Payload: 'T
+}
+
 type ClusterCommand =
     | StopScenario of scenarioName:string * reason:string
     | StopTest     of reason:string
